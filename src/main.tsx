add200904ed379b467ccb6e3886569023926bd0e
viruop <virajrai706@gmail.com>
2024-08-03 17:59:07 +0530

    remove deprecated methoda

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import PaymentGatewayComponent from "@/components/PaymentGateway";
 import "./index.css";
 import { PaymentGatewayProps } from "./types";
@@ -6,11 +6,13 @@ import { PaymentGatewayProps } from "./types";
 class PaymentGateway {
   private options: PaymentGatewayProps;
   private container: HTMLDivElement;
+  private root: ReactDOM.Root;
 
   constructor(options: PaymentGatewayProps) {
     this.options = options;
     this.container = document.createElement("div");
     document.body.appendChild(this.container);
+    this.root = ReactDOM.createRoot(this.container);
   }
 
   on(event: string, callback: (response: any) => void): void {
@@ -20,22 +22,22 @@ class PaymentGateway {
   }
 
   open(): void {
-    ReactDOM.render(
+    this.root.render(
       <PaymentGatewayComponent
         {...this.options}
         onClose={() => {
-          ReactDOM.unmountComponentAtNode(this.container);
+          this.root.unmount();
           document.body.removeChild(this.container);
           if (this.options.modal && this.options.modal.ondismiss) {
             this.options.modal.ondismiss();
           }
         }}
-      />,
-      this.container
+      />
     );
   }
 
   close(): void {
+    // this.root.unmount();
     document.body.removeChild(this.container);
   }
 }
